bf1af3131d7ea868b2ecd7263af186c3cf1d86d4
Code Subtle <korvanvitthal@gmail.com>
2025-11-10 16:48:45 +0530

    page.js updated!

--- next-js-intro/app/page.js
+++ next-js-intro/app/page.js
@@ -2,11 +2,11 @@
 import { useState } from "react";
 
 const page = () => {
-  const [name, setName] = useState("Vitthal")
+  const [name, setName] = useState("Minaxi")
   return (
-    <div>
-      <h1>Vitthal Korvan - Code Subtle</h1>
-      <button>Hello! {name}</button>
+    <div className="h-screen w-full bg-zinc-800 text-white">
+      <h1 className="text-4xl tracking-tighter">Vitthal Korvan - Code Subtle</h1>
+      <button className="px-6 py-3 bg-blue-700 rounded-4xl cursor-pointer">Hello! {name}</button>
     </div>
   );
 }
